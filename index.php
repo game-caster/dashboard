f9fcac44ed00d350af2666f5e7088d2e2496750f
opum2 <opum2@users.noreply.github.com>
2016-11-17 09:46:36 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+<title>Dashboard | LifeLikes</title>
+
+<style>
+body {
+  margin: 0px;
+  border: 0px;
+}
+#header {
+  width: 100%;
+  height: 120px;
+  background: #418fff;
+  color: white;
+}
+</style
+
+</head>
+<body>
+
+</body>
+</html>
